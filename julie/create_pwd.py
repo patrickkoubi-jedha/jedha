801089feb145a5c4515d4263fbb79055bbc4b00e
Patrick Koubi <patrickkoubi@gmail.com>
2026-02-04 15:51:18 +0100

    maj exo commentaire

--- julie/create_pwd.py
+++ julie/create_pwd.py
@@ -1,10 +1,15 @@
+#password valide
 sec_pwd="123toto"
+#flag pwd valide
 pwd_valid=False
 
+#boucle tant que pwd non valide
 while not pwd_valid:
     pwd = input("Entrez un password: ")
     pwd_valid=False
+    #comparaison de la saisie avec le pwd secret et mise a jour du flag
     pwd_valid=pwd==sec_pwd
+    #affichage du resultat
     if not pwd_valid:
         print("Password non valide, réessayez.") 
     else:
